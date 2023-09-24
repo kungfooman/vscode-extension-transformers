cc84f9af08c3c9da19dcd53a7e6ce86ca3fb1165
kungfooman <lama12345@gmail.com>
2023-09-24 12:42:43 +0200

    add config files

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/**@type {import('eslint').Linter.Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+	root: true,
+	extends: [
+		'eslint:recommended'
+	],
+	rules: {
+		'semi': [2, "always"]
+	}
+};
