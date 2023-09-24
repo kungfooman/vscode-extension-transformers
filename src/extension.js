f2dedfc3f08bdcc38cf169995d056cd9fdb8a053
kungfooman <lama12345@gmail.com>
2023-09-24 12:44:01 +0200

    small cleanup / fix

--- src/extension.js
+++ src/extension.js
@@ -1,7 +1,8 @@
 /**
- * @param {import('vscode').ExtensionContext} context 
+ * @param {import('vscode').ExtensionContext} context - The context.
  */
-module.exports.activate = async function activate(context) {
+async function activate(context) {
   (await import('./register-hello-world.mjs')).activate(context);
   (await import('./register-hello-tensor.mjs')).activate(context);
 }
+module.exports.activate = activate;
